919faa994fb4646d3227c282bfcf35ab015246c2
KirillBuchnev <150864272+KirillBuchnev@users.noreply.github.com>
2023-12-06 00:19:26 +0300

    uraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- zadanie_3.py
+++ zadanie_3.py
@@ -1,18 +1,39 @@
 try:
-    n = int(input("Введите количество стран:"))
+    n = int(input("Введите количество стран: "))
     if n<=0:
         raise ValueError
     country_and_rivers = {}
     for i in range(n):
-        country = input("Введите название страны:")
-        rivers = input("Введите список названий рек, разделенных пробелом: ").split()
+        country = input("Введите название страны: ") 
+        rivers = []
+        river_input = input("Введите названия рек через запятую: ")
+        river = ""
+        for bukva in river_input:
+            if bukva != ",":
+                river += bukva
+            else:
+                rivers.append(river)
+                river = ""
+        rivers.append(river)
         country_and_rivers[country] = rivers
-    river = input("Введите список  нужных вам названий рек, разделенных запятой: ").split(",")
-    for i in river:
-      for country, rivers in country_and_rivers.items():
-        if i in rivers:
-            print(f"Река {i} протекает в стране {country}")
+    river_list = [] 
+    river_input = input("Введите нужные вам реки через запятую: ")
+    river = ""
+    for bukva in river_input:
+        if bukva != ",":
+            river += bukva
         else:
-            print(f"Река {i} отсутствует в словаре стран и рек")
+            river_list.append(river)
+            river = ""
+    river_list.append(river)
+    for river in river_list:
+        river_found = False
+        for country, rivers in country_and_rivers.items():
+            if river in rivers:
+                print(f"Река {river} протекает в стране {country}")
+                river_found = True
+        if not river_found:
+            print(f"Река {river} отсутствует в словаре стран и рек")
 except:
     print("Введенно некорректное значение")
+
