d6401aecc2fb5f447d131ded76b3d444cf90d6a4
KirillBuchnev <150864272+KirillBuchnev@users.noreply.github.com>
2023-12-05 22:15:30 +0300

    i zabil

--- zadanie_3.py
+++ zadanie_3.py
@@ -8,16 +8,11 @@ try:
         rivers = input("Введите список названий рек, разделенных пробелом: ").split()
         country_and_rivers[country] = rivers
     river = input("Введите список  нужных вам названий рек, разделенных запятой: ").split(",")
-    for river in rivers:
+    for i in river:
       for country, rivers in country_and_rivers.items():
-        if river in rivers:
-            print(f"Река {river} протекает в стране {country}")
+        if i in rivers:
+            print(f"Река {i} протекает в стране {country}")
         else:
-            print(f"Река {river} отсутствует в словаре стран и рек")
+            print(f"Река {i} отсутствует в словаре стран и рек")
 except:
     print("Введенно некорректное значение")
-
-
-
-
-
