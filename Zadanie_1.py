c3597df03bcc0b725bb762aebe5ad3a469ad14b2
KirillBuchnev <150864272+KirillBuchnev@users.noreply.github.com>
2023-12-13 09:52:20 +0300

    1

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -1,22 +1,28 @@
 import random
 try:
-    def poisk_pervachkov_v_DSTU (spisok):
-        spisok_2 = []
-        for i in range(len(spisok)):
-            for j in range(len(spisok[i])):
-             if spisok[i][j] not in spisok_2:
-                spisok_2.append(spisok[i][j])
-        return spisok_2
     m = int(input("Введите значения M:"))
     n = int(input("Введите значения N:"))
     if n<=0 or m<=0:
         raise ValueError
     spisok = []
+    slovar = {}
     for i in range(m):
-        spisok.append([random.randint(0,10) for j in range(n)])
+        stroka = []
+        for j in range(n):
+            chisla = random.randint(0,10)
+            if chisla in slovar:
+                slovar[chisla] += 1
+            else:
+                slovar[chisla] = 1
+            stroka.append(chisla)
+        spisok.append(stroka)
     for i in range(m):
             print(spisok[i])
-    print("Неповторяющиеся элементы" , poisk_pervachkov_v_DSTU(spisok))  
+    otvet= []
+    for zhach, kolich in slovar.items():
+         if kolich == 1:
+              otvet.append(zhach)
+    print("Неповторяющиеся элементы" , otvet)
 except:
     print("Введены неверные параметры для двумерного списка")
 
